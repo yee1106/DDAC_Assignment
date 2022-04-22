e4fff134edf432e4101cc15c83cc8af57824546e
Chew Chang Wang <chewchangwang@gmail.com>
2022-04-22 15:13:37 +0800

    category update

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDAC_Assignment.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index(string page)
+        {
+            if (!string.IsNullOrEmpty(page))
+            {
+                ViewBag.pageName = page;
+            }
+            return View();
+        }
+    }
+}
