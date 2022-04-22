af729b39bff618d6ecdcbe75a15045280258d08b
Chia Leng <chialeng.2000@gmail.com>
2022-04-22 13:00:29 +0800

    admin send password reset link forgot password

--- Configuration.cs
+++ Configuration.cs
@@ -7,13 +7,13 @@ namespace DDAC_Assignment
 {
     public class Configuration
     {
-        public static string base_url = "https://xc50vkv0wa.execute-api.us-east-1.amazonaws.com/";
+        public static string base_url = "https://ybxr7veqs2.execute-api.us-east-1.amazonaws.com/";
 
         public class default_admin
         {
              public static string UserName = "ddac.enews@gmail.com";
              public static string Email = "ddac.enews@gmail.com";
-            public static string password = "DDACadmin123@";
+             public static string password = "DDACadmin123@";
         }
     }
 }
--- Models/APIs/Email.cs
+++ Models/APIs/Email.cs
@@ -9,6 +9,12 @@ namespace DDAC_Assignment.Models.APIs
     {
         static string email_api_root = "email/";
 
+        public static async Task<string> email_api_template(Dictionary<string, string> request_body, string api_name)
+        {
+            string url = email_api_root + api_name;
+            string response = await ApiTemplate.post(request_body, url);
+            return response;
+        }
         public static async Task<string> send_email_confirmation_api(string user_email, string user_full_name, string confirmation_link)
         {
             var request_body = new Dictionary<string, string>
@@ -18,8 +24,22 @@ namespace DDAC_Assignment.Models.APIs
                   { "confirmation_link", confirmation_link }
               };
 
-            string url = email_api_root + "send_confirmation_email";
-            string response = await ApiTemplate.post(request_body, url);
+            var response = await email_api_template(request_body, "send_confirmation_email");
+
+            return response;
+        }
+
+
+        public static async Task<string> send_password_reset_link_api(string user_email, string user_full_name, string reset_link)
+        {
+            var request_body = new Dictionary<string, string>
+              {
+                  { "email", user_email },
+                  { "full_name", user_full_name },
+                  { "reset_link", reset_link }
+              };
+
+            var response = await email_api_template(request_body, "send_password_reset_link");
 
             return response;
         }
