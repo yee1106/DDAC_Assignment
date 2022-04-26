a64c2a463ef8dd3dfc0773411b647813ea0f680c
Yee Chung <yee23700@gmail.com>
2022-04-26 22:30:23 +0800

    added comment model

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDAC_Assignment.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/customer/NewsViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DDAC_Assignment.Models.customer
+{
+    public class NewsViewModel
+    {
+        public Advertisement Advertisement { get; set; }
+        public News News { get; set; }
+    }
+}
